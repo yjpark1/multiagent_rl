81de0279ee39c84cce382dbe530e750624a53cdd
yjpark1 <dmqm.yjpark@gmail.com>
2018-10-23 11:42:00 +0900

    Update rl/model/ac_network_model_rdpg.py

--- rl/model/ac_network_model_rdpg.py
+++ rl/model/ac_network_model_rdpg.py
@@ -8,39 +8,56 @@ class TimeDistributed(nn.Module):
         super(TimeDistributed, self).__init__()
         self.module = module
 
-    def forward(self, x):
-        if len(x.size()) <= 2:
-            return self.module(x)
-            # reshape
-        elif len(x.size()) == 3:
+    def forward(self, x, h0=None):
+        if len(x.size()) == 3:
+            # TimeDistributed + Dense
             # shape: (seq_len, batch, features)
             seq_len, b, n = x.size(0), x.size(1), x.size(2)
             # merge batch and seq dimensions
-            x_reshape = x.contiguous().view(seq_len, b * n)
+            x_reshape = x.contiguous().view(seq_len * b, n)
 
         elif len(x.size()) == 4:
-            # shape: (seq_len, batch, documents, features)
+            # TimeDistributed + RNN
+            # shape: (seq_len, batch, agents, features)
             seq_len, b, n, d = x.size(0), x.size(1), x.size(2), x.size(3)
             # merge batch and seq dimensions
             x_reshape = x.contiguous().view(seq_len, b * n, d)
 
             # return
-        if hasattr(self.module, 'rnn'):
-            y, h = self.module(x_reshape)
-            # shape: (seq_len, batch x documents, features)
-            y = y.contiguous().view(seq_len, b, n, y.size()[-1])
-            # shape: (seq_len, batch, documents, features)
-
-            # shape: (batch, documents, features)
-            h = h.contiguous().view(1, b, n, h.size()[-1])
-            return y, h
-
-        else:
+        if isinstance(self.module, nn.Linear):
+            # TimeDistributed + Dense
             y = self.module(x_reshape)
             # We have to reshape Y
             # shape: (seq_len, batch, documents, features)
             y = y.contiguous().view(seq_len, b, n, y.size()[-1])
             return y
+
+        elif isinstance(self.module, nn.LSTM):
+            # TimeDistributed + RNN
+            y, (h, c) = self.module(x_reshape, h0)
+            x_reshape.size()
+            # shape: (seq_len, batch x agents, features)
+            y = y.contiguous().view(seq_len, b, n, y.size()[-1])
+            # shape: (seq_len, batch, agents, features)
+
+            # shape: (batch, agents, features)
+            h = h.contiguous().view(h.size()[0], b, n, h.size()[-1])
+            c = c.contiguous().view(c.size()[0], b, n, c.size()[-1])
+            return y, (h, c)
+
+        elif isinstance(self.module, nn.GRU):
+            # TimeDistributed + RNN
+            y, h = self.module(x_reshape, h0)
+            # shape: (seq_len, batch x agents, features)
+            y = y.contiguous().view(seq_len, b, n, y.size()[-1])
+            # shape: (seq_len, batch, agents, features)
+
+            # shape: (batch, agents, features)
+            h = h.contiguous().view(1, b, n, h.size()[-1])
+            return y, h
+
+        else:
+            raise ImportError('Not Supported Layers!')
 
 
 class ActorNetwork(nn.Module):
@@ -59,12 +76,12 @@ class ActorNetwork(nn.Module):
         super(ActorNetwork, self).__init__()
 
         self.nonlin = F.relu
-        self.dense1 = TimeDistributed(nn.Linear(input_dim, 64))
+        self.dense1 = nn.Linear(input_dim, 64)
+        self.lstmTime = TimeDistributed(nn.LSTM(64, 64, num_layers=1, bidirectional=False))
         # return sequence is not exist in pytorch. Instead, output will return with first dimension for sequences.
-        self.bilstm = nn.LSTM(64, 32, num_layers=1,
-                              batch_first=True, bidirectional=True)
-        self.dense2 = TimeDistributed(nn.Linear(64, out_dim))
-        self.dense3 = TimeDistributed(nn.Linear(64, input_dim))
+        self.bilstmAgent = TimeDistributed(nn.LSTM(64, 32, num_layers=1, bidirectional=True))
+        self.dense2 = nn.Linear(64, out_dim)
+        self.dense3 = nn.Linear(64, input_dim)
 
     def forward(self, obs):
         """
@@ -73,8 +90,12 @@ class ActorNetwork(nn.Module):
         Outputs:
             out (PyTorch Matrix): policy, next_state
         """
+        # obs: (time, batch, agent, feature)
         hid = F.relu(self.dense1(obs))
-        hid, _ = self.bilstm(hid, None)
+        hid, hcTime = self.lstmTime(hid, h0=None)
+        hid = hid.permute(2, 1, 0, 3)
+        hid, hcAgent = self.bilstmAgent(hid, h0=None)
+        hid = hid.permute(2, 1, 0, 3)
         hid = F.relu(hid)
         policy = self.dense2(hid)
         policy = nn.Softmax(dim=-1)(policy)
@@ -100,9 +121,9 @@ class CriticNetwork(nn.Module):
 
         self.nonlin = F.relu
         self.dense1 = TimeDistributed(nn.Linear(input_dim, 64))
+        self.lstmTime = nn.LSTM(64, 64, num_layers=1, bidirectional=False)
         # return sequence is not exist in pytorch. Instead, output will return with first dimension for sequences.
-        self.lstm = nn.LSTM(64, 64, num_layers=1,
-                            batch_first=True, bidirectional=False)
+        self.lstmAgent = nn.LSTM(64, 64, num_layers=1, bidirectional=False)
         self.dense2 = nn.Linear(64, out_dim)
         self.dense3 = nn.Linear(64, out_dim)
 
@@ -116,8 +137,21 @@ class CriticNetwork(nn.Module):
         """
         obs_act = torch.cat((obs, action), dim=-1)
         hid = F.relu(self.dense1(obs_act))
-        hid, _ = self.lstm(hid, None)
+        hid, _ = self.lstmTime(hid, None)
+        hid, _ = self.lstmAgent(hid, None)
         hid = F.relu(hid[:, -1, :])
         Q = self.dense2(hid)
         r = self.dense3(hid)
         return Q, r
+
+if __name__ == '__main__':
+    actor = ActorNetwork(input_dim=10, out_dim=5)
+    critic = CriticNetwork(input_dim=10, out_dim=1)
+
+    s = torch.randn(15, 32, 3, 10)
+    pred_actor = actor.forward(s)
+    pred_actor[0].size()
+    pred_actor[1].size()
+
+    a = torch.randn(15, 32, 3, 5)
+    pred_critic = critic.forward(s, a)
